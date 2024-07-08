7f6bb0801d311341d724f6f41720d262dedcdb83
chirag-kushwah <135162156+chirag-kushwah@users.noreply.github.com>
2024-07-08 10:58:28 -0700

    new

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //const Live_URL = "mongodb+srv://chiragkushwah06:ram123@cluster0.ytcxkya.mongodb.net/admissionPortal?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDB = () => {
-    return mongoose.connect(process.env.LOCAL_URL)
+    return mongoose.connect(process.env.LIVE_URL)
         .then(() => {
             console.log("Connect Successfully")
         }).catch((error) => {
